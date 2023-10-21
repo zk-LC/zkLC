95df7167afa1696bf7717713c892d415fe33ddb0
denosaurabh <denosaurabh@gmail.com>
2023-10-22 02:20:04 +0530

    fixed build error

--- client/lib/consts.ts
+++ client/lib/consts.ts
@@ -1,3 +1,4 @@
-export const ZKLC_CONTRACT_ADDRESS =
-  process.env.NEXT_PUBLIC_ZKLC_CONTRACT_ADDRESS;
-export const USDC_MOCK_ADDRESS = process.env.NEXT_PUBLIC_USDC_MOCK_ADDRESS;
+export const ZKLC_CONTRACT_ADDRESS = process.env
+  .NEXT_PUBLIC_ZKLC_CONTRACT_ADDRESS as `0x${string}`;
+export const USDC_MOCK_ADDRESS = process.env
+  .NEXT_PUBLIC_USDC_MOCK_ADDRESS as `0x${string}`;
